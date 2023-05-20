96b559bb34aad06fbbb14488109b96b39a449d7b
Phathdt <phathdt379@gmail.com>
2023-05-20 18:25:54 +0700

    update condition query list channel

--- services/channel_service/internal/modules/channel/channel_stranport/channel_grpc/list_hermes_channel_credential.go
+++ services/channel_service/internal/modules/channel/channel_stranport/channel_grpc/list_hermes_channel_credential.go
@@ -2,7 +2,6 @@ package channel_grpc
 
 import (
 	"context"
-	"encoding/json"
 
 	"jetshop/common"
 	jetshop_proto "jetshop/proto/out/proto"
@@ -12,18 +11,13 @@ import (
 )
 
 func (s *channelGrpcServer) ListHermesChannelCredential(ctx context.Context, request *jetshop_proto.ChannelListHermesCredentialRequest) (*jetshop_proto.ChannelListHermesCredentialResponse, error) {
-	cond := make(map[string]interface{})
-	if err := json.Unmarshal([]byte(request.Cond), &cond); err != nil {
-		panic(err)
-	}
-
 	sc := s.sc
 
 	db := sc.MustGet(common.KeyCompGorm).(gormc.GormComponent).GetDB()
 	store := channel_storage.NewSqlStore(db)
 	repo := channel_repo.NewRepo(store)
 
-	credentials, err := repo.ListChannelCredentials(ctx, cond)
+	credentials, err := repo.ListChannelCredentials(ctx, map[string]interface{}{"is_enabled": request.IsEnabled})
 	if err != nil {
 		panic(err)
 	}
