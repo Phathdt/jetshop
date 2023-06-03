04edf60238a09e234e918213644f21c60731b5c0
Phathdt <phathdt379@gmail.com>
2023-06-03 21:07:20 +0700

    init update thread consumer

---
+++ services/chat_service/cmd/chat_update_thread_consumer/internal/root.go
@@ -0,0 +1,83 @@
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"jetshop/services/chat_service/internal/modules/chat_transport/chatconsumer"
+	"jetshop/shared/appgrpc"
+	"jetshop/shared/common"
+	"jetshop/shared/sctx"
+	"jetshop/shared/sctx/component/discovery/consul"
+	"jetshop/shared/sctx/component/ginc"
+	smdlw "jetshop/shared/sctx/component/ginc/middleware"
+	"jetshop/shared/sctx/component/gormc"
+	"jetshop/shared/sctx/component/tracing"
+	"jetshop/shared/sctx/component/watermillapp"
+	"jetshop/shared/sctx/component/watermillapp/natspub"
+	"jetshop/shared/sctx/component/watermillapp/natsrouter"
+)
+
+const (
+	serviceName = "chat_update_thread_consumer"
+	version     = "1.0.0"
+)
+
+func newServiceCtx() sctx.ServiceContext {
+	return sctx.NewServiceContext(
+		sctx.WithName(serviceName),
+		sctx.WithComponent(ginc.NewGin(common.KeyCompGIN)),
+		sctx.WithComponent(gormc.NewGormDB(common.KeyCompGorm, "")),
+		sctx.WithComponent(consul.NewConsulComponent(common.KeyCompConsul, serviceName, version, 3000)),
+		sctx.WithComponent(tracing.NewTracingClient(common.KeyCompJaeger, serviceName)),
+		sctx.WithComponent(appgrpc.NewChannelClient(common.KeyCompChannelClient)),
+		sctx.WithComponent(natsrouter.NewNatsRouter(common.KeyCompNatsSub)),
+		sctx.WithComponent(natspub.NewNatsPub(common.KeyCompNatsPub)),
+	)
+}
+
+var rootCmd = &cobra.Command{
+	Use:   serviceName,
+	Short: fmt.Sprintf("start %s", serviceName),
+	Run: func(cmd *cobra.Command, args []string) {
+		serviceCtx := newServiceCtx()
+
+		logger := sctx.GlobalLogger().GetLogger("service")
+
+		time.Sleep(time.Second * 5)
+
+		if err := serviceCtx.Load(); err != nil {
+			logger.Fatal(err)
+		}
+
+		subscriber := serviceCtx.MustGet(common.KeyCompNatsSub).(watermillapp.Subscriber)
+		subscriber.AddNoPublisherHandler("update_thread", "update_thread", chatconsumer.UpdateThread(serviceCtx))
+
+		ginComp := serviceCtx.MustGet(common.KeyCompGIN).(ginc.GinComponent)
+
+		router := ginComp.GetRouter()
+		router.Use(gin.Recovery(), gin.Logger(), smdlw.Recovery(serviceCtx), otelgin.Middleware(serviceName))
+
+		router.GET("/ping", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"data": "pong"})
+		})
+
+		if err := router.Run(fmt.Sprintf(":%d", ginComp.GetPort())); err != nil {
+			logger.Fatal(err)
+		}
+	},
+}
+
+func Execute() {
+	rootCmd.AddCommand(outEnvCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ services/chat_service/cmd/chat_update_thread_consumer/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	cmd "jetshop/services/chat_service/cmd/chat_update_thread_consumer/internal"
+)
+
+func main() {
+	cmd.Execute()
+}
